225ac8947d47e3f3df9694914fce1384663a55c6
rajesh <krajeshgowri7032@mail.com>
2022-09-21 15:24:51 +0530

    add files

---
+++ fabonacci.js
@@ -0,0 +1,19 @@
+// const x =10;
+// let n1=0, n2=1, nextTerm;
+// console.log('fabonacci series');
+
+// for(let i=0; i<=x; i++){
+//     console.log(n1);
+//     nextTerm=n1+n2;
+//     n1=n2;
+//     n2=nextTerm;
+// }
+
+const num=10;
+let n1=0, n2=1, nextTerm;
+for (i=1;i<=num;i++){
+    console.log(n1);
+    nextTerm=n1+n2;
+    n1=n2;
+    n2=nextTerm;
+}
